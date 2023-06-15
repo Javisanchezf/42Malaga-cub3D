63212859d42425f51af218464e1224e8abe6f17a
javiersa <javiersa@student.42malaga.com>
2023-06-15 21:39:07 +0200

    [2023-06-15 21:39:06] - Little changes by javiersa

--- src/map_parse.c
+++ src/map_parse.c
@@ -6,51 +6,46 @@
 /*   By: javiersa <javiersa@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 21:07:49 by javiersa          #+#    #+#             */
-/*   Updated: 2023/06/15 21:13:21 by javiersa         ###   ########.fr       */
+/*   Updated: 2023/06/15 21:38:21 by javiersa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-// int	ft_check_all_data_extract(t_cub3data *data)
-// {
-// 	return (!data->ids.no || !data->ids.so || !data->ids.we || !data->ids.ea || !data->ids.f || !data->ids.c);
-// }
-
-void	ft_extract_ids_aux()
+char	*ft_extract_ids(int fd, t_cub3data *data, char *line)
 {
-	
-}
-
-char	*ft_extract_ids(int fd, char *aux, t_cub3data *data)
-{
-	char	*line;
-
-	line = get_next_line(fd);
-	if (!line)
-		ft_error("Error\nThe file does not contain a valid map.", 1, aux);
 	while (line)
 	{
 		if (data->ids.no && data->ids.so && data->ids.we \
 		&& data->ids.ea && data->ids.f && data->ids.c)
 			break ;
-		if ()
+		else if (ft_strncmp("NO ", line, 3) == 0)
+			data->ids.no = ft_strdup(line);
+		else if (ft_strncmp("SO ", line, 3) == 0)
+			data->ids.so = ft_strdup(line);
+		else if (ft_strncmp("WE ", line, 3) == 0)
+			data->ids.we = ft_strdup(line);
+		else if (ft_strncmp("EA ", line, 3) == 0)
+			data->ids.ea = ft_strdup(line);
+		else if (ft_strncmp("F ", line, 2) == 0)
+			data->ids.f = ft_strdup(line);
+		else if (ft_strncmp("C", line, 2) == 0)
+			data->ids.c = ft_strdup(line);
+		ft_free_and_null((void **)&line);
+		line = get_next_line(fd);
 	}
+	if (!line)
+		ft_error ("Error\nThe file doesn't contain valid map", 6, data->ids.no, \
+		data->ids.so, data->ids.we, data->ids.ea, data->ids.f, data->ids.c);
+	return (line);
 }
 
 void	ft_readmap(int fd, t_cub3data *data)
 {
 	char	*line;
-	char	*aux;
 
 	data->map_height = 0;
-	aux = ft_calloc(1, 1);
-	if (!aux)
-		ft_error("Error\nProblem allocating memory, try freeing up space.", 0);
-	line = ft_extract_ids();
-	// line = get_next_line(fd);
-	// if (!line)
-	// 	ft_error("Error\nThe file does not contain a valid map.", 1, aux);
+	line = ft_extract_ids(fd, data, get_next_line(fd));
 	data->map_width = ft_count_words(line);
 	while (line && ++data->map_height)
 	{
