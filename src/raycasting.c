eb1ab7c3d91c277e06cf2a8423a12fbac6bec370
javisanchezf <javiersa@student.42malaga.com>
2023-10-27 10:52:42 +0200

    [2023-10-27 10:52:42] - Little changes by javiersa

--- src/raycasting.c
+++ src/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: javiersa <javiersa@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 18:40:45 by antdelga          #+#    #+#             */
-/*   Updated: 2023/10/27 10:46:28 by javiersa         ###   ########.fr       */
+/*   Updated: 2023/10/27 10:51:46 by javiersa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,9 +78,9 @@ void	raycasting(t_cub3data *data, t_coords pos)
 		aux.otro = (aux.p.y * data->width * BLOCKSIZE + aux.p.x) * 4;
 		aux.este = (aux.p.y * data->width * BLOCKSIZE + aux.p.x) % BLOCKSIZE;
 		aux.norte = aux.p.y % BLOCKSIZE;
-		if (aux.este <= 2 && data->map_close.img[aux.otro - 8 * 4] != 255)
+		if (aux.este <= 2 && data->map_close.img[aux.otro - 2 * 4] != 255)
 			picasso(aux.dist, aux.iter * (WIDTH / (ANGLE * SAMPLE)), data, data->color.red);
-		else if (aux.este >= BLOCKSIZE - 2 && data->map_close.img[aux.otro + 8 * 4] != 255)
+		else if (aux.este >= BLOCKSIZE - 2 && data->map_close.img[aux.otro + 2 * 4] != 255)
 			picasso(aux.dist, aux.iter * (WIDTH / (ANGLE * SAMPLE)), data, data->color.golden);
 		else if (aux.norte <= 2)
 			picasso(aux.dist, aux.iter * (WIDTH / (ANGLE * SAMPLE)), data, data->color.blue);
